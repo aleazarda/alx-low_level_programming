e254c7b58a012a189d4d8e87da2656d0770b9a24
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-06-04 21:57:01 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers since 0 to 100
+  * main - Prints the numbers since 0 to 10
   *
   * Return: Always (Success)
   */
@@ -9,7 +9,7 @@ int main(void)
 {
 	char n;
 
-	for (n = '0'; n <= '100'; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
