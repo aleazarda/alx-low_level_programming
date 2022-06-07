c1d0f85b8e32b002aae0f68afc8f02288bd89e6d
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-06-07 23:07:20 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,20 +8,15 @@
 void print_alphabet_x10(void)
 {
 	int j = 0;
-	
-	while (j < 10)
-		
+	while (j < 10)	
 	{
 		char alp = 'a';
-    while (alp <= 'z')
-    {
+		while (alp <= 'z')
+		{
 		_putchar (alp);
 		alp++;
-    }
-
-	
+		}
 	_putchar ('\n');
-	j++;
-		
+	j++;	
 	}
 }
