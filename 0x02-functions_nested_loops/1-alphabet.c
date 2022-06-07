568217e4ab9c0048a5ba582ce4962f14e066050f
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-06-07 22:20:03 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+  char alp="a"
+    while (alp="a"; alp="<=z"){
+    putchar(alp);
+    }
+    putchar(\n);
+}
