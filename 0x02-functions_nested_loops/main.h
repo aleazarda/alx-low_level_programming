ea5a2043b360509a1d37b6a8957d21e08edf28d6
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-06-07 22:23:14 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,56 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 /*
-Main.h
-Written by Matthew Fisher
+ * File: main.h
+ *
+ * Desc: Header file containing declartions for all function
+ */
 
-Main.h is included by all source files and includes every header file in the correct order.
-*/
-#pragma once
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_teble(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n);
 
-//
-// Config.h includes a series of #defines used to control compiling options
-//
-#include "Config.h"
-
-//
-// Engine.h includes everything that rarely changes between applications, such as vector/Matrix4 libraries,
-// OpenGL/DirectX graphics devices, software rasterizers, etc.
-//
-#include "Engine.h"
-
-#include "Constants.h"
-
-#include "BaseCodeDLL.h"
-
-#include "AppParameters.h"
-
-#include "Enums.h"
-
-#include "CardDatabase.h"
-#include "GameData.h"
-
-#include "State.h"
-#include "BuyAgenda.h"
-#include "Player.h"
-#include "TestChamber.h"
-#include "MetaTestChamber.h"
-#include "AIUtility.h"
-
-#include "Event.h"
-#include "EventBase.h"
-#include "EventIntrigue.h"
-#include "EventSeaside.h"
-#include "EventAlchemy.h"
-#include "EventProsperity.h"
-#include "EventCustom.h"
-
-#include "CardEffect.h"
-#include "CardEffectBase.h"
-#include "CardEffectIntrigue.h"
-#include "CardEffectSeaside.h"
-#include "CardEffectAlchemy.h"
-#include "CardEffectProsperity.h"
-#include "CardEffectCustom.h"
-
-#include "DominionGame.h"
-
-#include "App.h"
+#endif
