32c5b68ea5fbf0d342ef405eb20bb180804c9a3c
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-06-07 22:51:39 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - ALX project code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+   char alp = 'a, j';
+
+	while (alp <= 'z')
+	{
+    while (j <= 10)
+    {
+		_putchar (alp);
+		alp++;
+    }
+
+	}
+	_putchar ('\n');
+}
