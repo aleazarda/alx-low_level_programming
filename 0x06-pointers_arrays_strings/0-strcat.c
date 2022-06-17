8fb1e66c2d7f6083d804e8657018abf8b00ec1aa
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-06-17 10:22:56 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,22 +7,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+int i, j;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
 }
-© 2022 GitHub, Inc.
+
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
